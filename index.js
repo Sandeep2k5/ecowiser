326e49cf098ba148a0a946ad8fa2e5f2916f6479
Sandeep U <159121732+Sandeep2k5@users.noreply.github.com>
2024-12-07 23:23:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Import the 'createRoot' method from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root")); // Create root using 'createRoot'
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
